6065bd24b072b1a8b96d3a6f3df918579b344ea5
Elliot Blackburn <elliot.blackburn@gmail.com>
2017-05-31 17:47:52 +0100

    nvm -> n for node version management

--- node/install.sh
+++ node/install.sh
@@ -1,4 +1,15 @@
-if test ! $(which mdpdf)
-then
-  sudo npm install mdpdf -g
-fi
+#!/bin/sh
+#
+# Install tj/n, a node version manager package. This also comes
+# bundled with a script for upgrading and uninstall - https://github.com/mklement0/n-install
+
+curl -sL https://git.io/n-install | bash -s -- -q -
+
+# Install the latest version of node, quietly if possible
+echo "› n latest"
+n latest
+# Use that install, this command is a bit silly really.
+n latest
+
+# Yarn requires node, so make sure it's installed afterwards
+brew yarn
