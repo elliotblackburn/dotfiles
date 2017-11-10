641b8e1d54d36aaffc97b6eab2e4f951f35df6ed
Elliot Blackburn <Elliot.blackburn@gmail.com>
2017-11-10 13:39:08 +0000

    Add git completion, sort of

--- README.md
+++ README.md
@@ -37,6 +37,7 @@ There's a few special files in the hierarchy.
   your `$HOME`. This is so you can keep all of those versioned in your dotfiles
   but still keep those autoloaded files in your home directory. These get
   symlinked in when you run `script/bootstrap`.
+- **topic/*.completion.zsh**: Any files ending in completion.zsh get loaded last so that they get loaded after we set up zsh autocomplete functions.
 
 ## install
 
--- git/completion.zsh
+++ git/completion.zsh
@@ -1,9 +1,12 @@
-# Uses git's autocompletion for inner commands. Assumes an install of git's
-# bash `git-completion` script at $completion below (this is where Homebrew
-# tosses it, at least).
-completion='$(brew --prefix)/share/zsh/site-functions/_git'
+compdef g=git
 
-if test -f $completion
-then
-  source $completion
-fi
+zstyle ':completion:*:*:g*:*' user-commands \
+                   amend:"Use the last commit message and amend your stuffs" \
+                   ci:"View CI status for a ref" \
+                   conflicts:"Show list of files in a conflict state" \
+                   credit:"Quick way to credit an author on the latest commit" \
+                   edit-conflicts:"Open files with conflicts in $EDITOR" \
+                   pr:"Start or open a pull request for the current branch" \
+                   rank-contributors:"Rank contributors by total size of diffs" \
+                   undo:"Undo your last commit, but don't throw away your changes" \
+                   wtf:"Display the state of your repository in a readable, easy-to-scan format." \
