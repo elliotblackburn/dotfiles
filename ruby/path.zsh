881384043dce2e242443876f36790d8204c72a84
Elliot Blackburn <Elliot.blackburn@gmail.com>
2017-11-13 10:47:37 +0000

    Better history!

--- ruby/path.zsh
+++ ruby/path.zsh
@@ -1,2 +1,8 @@
 # Add RVM to PATH for scripting
 export PATH="$PATH:$HOME/.rvm/bin"
+
+# Lunchy for systemctl handling with nicer commands
+LUNCHY_DIR=$(dirname `gem which lunchy`)/../extras
+if [ -f $LUNCHY_DIR/lunchy-completion.zsh ]; then
+  . $LUNCHY_DIR/lunchy-completion.zsh
+fi
