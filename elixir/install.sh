6fca2d3d8a6a03f481fb98f9202dcc89854c7f43
Elliot Blackburn <elliot.blackburn@gmail.com>
2018-07-10 13:08:42 +0100

    Only install elixir tools if they don't already exist

--- elixir/install.sh
+++ elixir/install.sh
@@ -1,11 +1,15 @@
 # Clone exenv for elixir environment management
 # this does not handle erlang. That is done via kerl
 # which is installed via brew.
-git clone git://github.com/mururu/exenv.git ~/.exenv
+if [ ! -e ~/.exenv ]; then
+    git clone git://github.com/mururu/exenv.git ~/.exenv
+fi
 
 # Exenv is added to the path by path.zsh in this dir
 
 # Exenv is initialised in zshrc.
 
 # Now setup the elixir-build plugin
-git clone git://github.com/mururu/elixir-build.git ~/.exenv/plugins/elixir-build
+if [ ! -e ~/.exenv/plugins/elixir-build ]; then
+    git clone git://github.com/mururu/elixir-build.git ~/.exenv/plugins/elixir-build
+fi
