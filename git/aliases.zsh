79d9c025ca539f638163958ae7aa667e1dd73314
Elliot Blackburn <Elliot.blackburn@gmail.com>
2018-10-25 09:49:26 +0100

    Clean up git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,16 +1,15 @@
 # The rest of my fun git aliases
-alias gl='git lg' # gitconfig.symlink
-alias gup='git up' # gitconfig.symlink
-alias gp='git push origin HEAD'
+alias gl='git lg'
+alias gup='git up'
 
 # Remove `+` and `-` from start of diff lines; just rely upon color.
 alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
 
 alias gc='git commit'
 alias gca='git commit -a'
-alias gco='git checkout'
-alias gcob='git cob' # gitconfig.symlink
+alias gac='git add -A && git commit -m'
+alias gco='git co'
+alias gcob='git cob'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb'
-alias gac='git add -A && git commit -m'
