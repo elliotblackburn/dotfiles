7592b1c5cd37a84d17d3c093fcd9c049a78e9f30
Elliot Blackburn <elliot.blackburn@just-eat.com>
2019-01-15 11:39:30 +0000

    fix g completion

--- git/completion.zsh
+++ git/completion.zsh
@@ -1,12 +1,12 @@
-compdef g=git
-
 zstyle ':completion:*:*:g*:*' user-commands \
-                   amend:"Use the last commit message and amend your stuffs" \
-                   ci:"View CI status for a ref" \
-                   conflicts:"Show list of files in a conflict state" \
-                   credit:"Quick way to credit an author on the latest commit" \
-                   edit-conflicts:"Open files with conflicts in $EDITOR" \
-                   pr:"Start or open a pull request for the current branch" \
-                   rank-contributors:"Rank contributors by total size of diffs" \
-                   undo:"Undo your last commit, but don't throw away your changes" \
-                   wtf:"Display the state of your repository in a readable, easy-to-scan format." \
+    amend:"Use the last commit message and amend your stuffs" \
+    ci:"View CI status for a ref" \
+    conflicts:"Show list of files in a conflict state" \
+    credit:"Quick way to credit an author on the latest commit" \
+    edit-conflicts:"Open files with conflicts in $EDITOR" \
+    pr:"Start or open a pull request for the current branch" \
+    rank-contributors:"Rank contributors by total size of diffs" \
+    undo:"Undo your last commit, but don't throw away your changes" \
+    wtf:"Display the state of your repository in a readable, easy-to-scan format." \
+
+compdef '_dispatch git git' g
